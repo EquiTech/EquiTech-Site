9c2f579a1e4388bba56b6e2884a542ce7d17f00e
eander16 <eric.anderson1@maconstate.edu>
2012-11-30 14:53:49 -0500

    Gave up on tab format, made each media its own page, added contact page (link clickable) added links to all on each page, changed footer to display 2 more groups.

--- feedback.php
+++ feedback.php
@@ -5,7 +5,7 @@
 	<meta name="description" content="EquiTech Studios Feedback Complete" />
 	<meta name="keywords" content="EquiTech Studios, feedback complete, will contact soon" />
 	<meta name="author" content="Group 2" />
-	<meta charset="utf-8">		
+	<meta charset="utf-8">
 	<link href="css/EquiTech.css" rel="stylesheet" type="text/css" />
 	<link href="css/jquery-ui-1.9.2.custom.css" rel="stylesheet" type="text/css" />
 	<script src="http://code.jquery.com/jquery-1.8.3.js"></script>
@@ -15,8 +15,8 @@
 	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js">
 	</script>
 	<link rel="stylesheet" type="text/css" href="ie.css" />
-	<![endif]-->	
-	
+	<![endif]-->
+
 	<script>
     $(function() {
         $( "input[type=submit],button" )
@@ -34,43 +34,48 @@
 			<header>
 			<hgroup>
 			<h1>EquiTech Studios</h1>
-				<h2>Gaming is our passion</h2>					
+				<h2>Gaming is our passion</h2>
 			</hgroup>
 					<form id="gsearch" action="http://studentweb.maconstate.edu/john.wright2/Group_Project2_final/search.html">
 					  <input value="015502771783107399443:ycdgt0vs6mc" name="cx" type="hidden"/>
 					  <input name="q" type="search" placeholder="Search the site" />
 					  <input value="Go" name="sa" type="submit" class="searchbutton" />
-					</form>	
+					</form>
 			</header>
 		<nav id="navbar">
-		 <ul> 
-			<li><a href="index.html">Home</a></li> 
+		 <ul>
+			<li><a href="index.html">Home</a></li>
 			<li><a href="games.html">Games</a>
-			  <ul> 
+			  <ul>
 				  <li><a href="nothingness.html">Nothingness</a></li>
 				  <li><a href="nothingness2.html">Nothingness 2</a></li>
 				  <li><a href="inproduction.html">Project"Unknown"</a></li>
-			  </ul> 
-			</li> 
+			  </ul>
+			</li>
 			<li><a href="media.html">Media</a>
+			 <ul>
+			 	  <li><a href="medianothingness.html">Nothingness</a></li>
+			 	  <li><a href="medianothingness2.html">Nothingness 2</a></li>
+			 	  <li><a href="mediaunknown.html">Project Uknown</a></li>
+			 </ul>
 			</li>
-			<li><a href="donate.html">Donate</a> 
+			<li><a href="donate.html">Donate</a>
 			</li>
-			<li><a href="#">Contact Us</a> 
-			  <ul> 
-				<li><a href="feedback.php">Feedback</a></li> 
-				<li><a href="mailto:eric.anderson1@maconstate.edu">Eric Anderson</a></li> 
-				<li><a href="mailto:kevin.hawkins@maconstate.edu">Kevin Hawkins</a></li> 
-				<li><a href="mailto:john.wright2@maconstate.edu">John Wright</a></li> 		
-			  </ul> 
-			</li> 
+			<li><a href="contactus.html">Contact Us</a>
+			  <ul>
+				<li><a href="feedback.php">Feedback</a></li>
+				<li><a href="mailto:eric.anderson1@maconstate.edu">Eric Anderson</a></li>
+				<li><a href="mailto:kevin.hawkins@maconstate.edu">Kevin Hawkins</a></li>
+				<li><a href="mailto:john.wright2@maconstate.edu">John Wright</a></li>
+			  </ul>
+			</li>
 		  </ul>
-		</nav>		
+		</nav>
 			<section class="content">
 				<bcrumbs>
 				<a href="index.html">Home</a>&raquo;&raquo;<a href="feedback.php">Feedback</a>
 				</bcrumbs>
-				
+
 				  <br/>
 				  <br/>
 					<form method="POST" action="feedback.php">
@@ -81,129 +86,129 @@
 					  <label for="myEmail">E-mail:</label>
 					  <input type="email" name="myEmail" id="myEmail" required="required">
 					  <label for="myPhone">Phone:</label>
-					  <input type="tel" name="myPhone" id="myPhone">		  
+					  <input type="tel" name="myPhone" id="myPhone">
 					  <label for="myComments">Comments:</label>
 					  <textarea name="myComments" id="myComments" rows="2" cols="20"></textarea>
 					  <label for="human">*What is 2+2? (Anti-spam)</label>
-					  <input id="human" type="text" name="human" placeholder="Type Here">					  
+					  <input id="human" type="text" name="human" placeholder="Type Here">
 					  <input id="mySubmit" name="mySubmit" type="submit" value="Submit">
 					</form>
-					
+
 						<div id="formResponse">
 						<?php
 
 								//Declare and check form vars
-								if (isset($_POST["myFName"])) 
+								if (isset($_POST["myFName"]))
 								{
 								  $fname = $_POST["myFName"];
 								  //echo $fname;
 								  //echo " is your First Name";
-								} 
-								else 
+								}
+								else
 								{
 								  $fname = null;
 								  //echo "no First Name supplied";
 								}
-								
-								if (isset($_POST["myLName"])) 
+
+								if (isset($_POST["myLName"]))
 								{
 								  $lname = $_POST["myLName"];
 								 // echo $lname;
 								 // echo " is your Last Name";
-								} 
-								else 
+								}
+								else
 								{
 								  $lname = null;
 								 // echo "no Last Name supplied";
 								}
-								
-								if (isset($_POST["myPhone"])) 
+
+								if (isset($_POST["myPhone"]))
 								{
 								  $phone = $_POST["myPhone"];
 								//  echo $phone;
 								//  echo " is your Phone";
-								} 
-								else 
+								}
+								else
 								{
 								  $phone = null;
 								//  echo "no Phone Number supplied";
-								}	
-								
-								if (isset($_POST["myEmail"])) 
+								}
+
+								if (isset($_POST["myEmail"]))
 								{
 								  $email = $_POST["myEmail"];
 								//  echo $email;
 								//  echo " is your Email Name";
-								} 
-								else 
+								}
+								else
 								{
 								  $email = null;
 								//  echo "no Email supplied";
-								}			
-								
-								if (isset($_POST["myComments"])) 
+								}
+
+								if (isset($_POST["myComments"]))
 								{
 								  $message = $_POST["myComments"];
 								//  echo $message;
 								//  echo " is your Comment";
-								} 
-								else 
+								}
+								else
 								{
 								  $message = null;
 								//  echo "no Comment supplied";
-								}							
-								
-								if (isset($_POST["myEmail"])) 
+								}
+
+								if (isset($_POST["myEmail"]))
 								{
 								  $from = $_POST["myEmail"];
 								//  echo $from;
 								//  echo " is your From email";
-								} 
-								else 
+								}
+								else
 								{
 								  $from = null;
 								//  echo "no From Email supplied";
-								}	
+								}
 
-								$to = 'equitechstudios@gmail.com'; 
+								$to = 'equitechstudios@gmail.com';
 								$subject = 'Equitech Studios Web Message';
-								
-								if (isset($_POST["human"])) 
+
+								if (isset($_POST["human"]))
 								{
 								  $human = $_POST["human"];
 								  //echo $human;
 								  //echo "is your Human input";
-								} 
-								else 
+								}
+								else
 								{
 								  $human = null;
 								//  echo "no Human supplied";
-								}	
+								}
 
-								if (isset($_POST["mySubmit"])) 
+								if (isset($_POST["mySubmit"]))
 								{
 								  $mySubmit = $_POST["mySubmit"];
 								 // echo $mySubmit;
 								//  echo " is your Submit button";
-								} 
-								else 
+								}
+								else
 								{
 								  $mySubmit = null;
 								//  echo "no Submit Button";
-								}							
-								
+								}
+
 								//message body
 								$body = "From: $fname $lname\n Phone: $phone\n E-Mail: $email\n Message:\n $message";
-								
+
 								//test if submit and required fields not null
 								if ($mySubmit) {
 									if ($fname != '' && $lname != '' && $email != '') {
-										if ($human == '4') {				 
-											if (mail ($to, $subject, $body, $from)) { 
+										if ($human == '4') {
+											if (mail ($to, $subject, $body, $from)) {
 											echo '<h2> We will be contacting you soon! </h2> <br> <p>Your message has been sent!</p>';
-										} else { 
-											echo '<p>Something went wrong, go back and try again!</p>'; 
-										} 
+										} else {
+											echo '<p>Something went wrong, go back and try again!</p>';
+										}
 									} else if ($mySubmit && $human != '4') {
 										echo '<p>You answered the anti-spam question incorrectly!</p>';
 									}
@@ -214,14 +219,14 @@
 
 					?>
 					</div>
-			</section>		
+			</section>
 
-			
+
 		<footer>
 				<ul>
 				  <li class="nodec">Contact Us</li>
 				  <li><a href="mailto:eric.anderson1@maconstate.edu">Eric Anderson</a><br></li>
-				  <li><a href="mailto:kevin.hawkins@maconstate.edu">Kevin Hawkins</a></li> 
+				  <li><a href="mailto:kevin.hawkins@maconstate.edu">Kevin Hawkins</a></li>
 				  <li><a href="mailto:john.wright2@maconstate.edu">John Wright</a></li>
 				</ul>
 				<ul>
@@ -237,19 +242,28 @@
 				  <li class="nodec">Site Links</li>
 				  <li><a href="index.html">Home</a></li>
 				  <li><a href="games.html">Games</a></li>
-				  <li><a href="nothingness.html">Nothingness</a></li>
-				  <li><a href="nothingness2.html">Nothingness 2</a></li>
-				  <li><a href="inproduction.html">Project"Unknown"</a></li>		
 				  <li><a href="media.html">Media</a></li>
 				  <li><a href="donate.html">Donate</a></li>
 				  <li><a href="feedback.php">Feedback</a></li>
 				</ul>
-				
+				<ul>
+				  <li class="nodec">Game Links</li>
+				  <li><a href="nothingness.html">Nothingness</a></li>
+				  <li><a href="nothingness2.html">Nothingness 2</a></li>
+				  <li><a href="inproduction.html">Project"Unknown"</a></li>
+				</ul>
+				<ul>
+				  <li class="nodec">Media Links</li>
+				  <li><a href="medianothingness.html">Nothingness</a></li>
+				  <li><a href="medianothingness2.html">Nothingness 2</a></li>
+			 	  <li><a href="mediaunknown.html">Project Uknown</a></li>
+			 	</ul>
+
 			<p id="footerCenter">All rights are reserved by their respected enities. Copyright &copy; 2012</p>
 			<datemoded><p id="footerCenter">Page last modified: <script> document.write(document.lastModified);</script></p></datemoded>
-		</footer>	
+		</footer>
 				<!-- div for siteim chat image -->
-				<div id="SITEHELP_CONTAINER_175" class="SITEHELP_CONTAINER_175"></div> 
+				<div id="SITEHELP_CONTAINER_175" class="SITEHELP_CONTAINER_175"></div>
 	</div>
 			<!-- JavaScript for siteim chat -->
 			<script type="text/javascript"> document.write('<scri' + 'pt type="text/javas' + 'cript" language="javas' + 'cript" src="' + top.location.protocol + '//c.sitehelp.im/code.cgi?c=175' + (top.location.protocol=='http:' ? '' : '&ssl=1') + '&rnd=' + Math.random() + '"></sc' + 'ript>'); </script>
