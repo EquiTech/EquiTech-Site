195c1caf4bebf0b0b29c6ecdca98834b28446fd5
John Wright <john@sacrarium.org>
2012-12-06 14:49:16 -0500

    update mostly to work with ie8, may need more testing.
    add modernizr js
    add ie8.css
    add message if less than ie8 to upgrade browser

--- feedback.php
+++ feedback.php
@@ -121,6 +121,9 @@
 				</div>
 			<section class="content">
 				<h2>Thank you for your contribution.</h2>
+					<!--[if lt IE 8]>
+						<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+					<![endif]-->				
 					<div id="formResponse">
 						<?php
 								//Declare and check form vars
